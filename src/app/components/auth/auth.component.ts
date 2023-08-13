5e28fe61fa856bfd6d54f3377c9611dc389ccd0b
Akaki Gogokhia <agogokhia@europebet.com>
2023-08-13 13:01:48 +0400

    auth styles

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from './auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
