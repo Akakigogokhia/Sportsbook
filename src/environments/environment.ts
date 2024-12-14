5697df9c7589442d9d96119afc0e4c5461d04f9f
Akakigogokhia <84267521+Akakigogokhia@users.noreply.github.com>
2024-12-14 13:53:20 +0400

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  firebaseApiKey: 'AIzaSyBKo9GNVvS8SUm99UGwXCkw760cI8cMHlc',
-  rapidApiKey: '244069d56cmshab8b2bb779ff79dp194954jsn16afcb16b9b3',
-};
